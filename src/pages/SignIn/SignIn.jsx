2d23e8f58deda41331f61f327b4a02b577d79c76
samin710 <smsaminii112@gmail.com>
2025-06-16 19:55:13 +0600

    design almost done

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -4,6 +4,10 @@ import { toast } from "react-toastify";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { AuthContext } from "../../providers/AuthContext";
+import { motion } from "framer-motion";
+import GradientText from "../../animations/GradientText/GradientText";
+import signInLottie from "../../assets/lottie/signinLottie.json";
+import Lottie from "lottie-react";
 
 const SignIn = () => {
   const { signin, googleSignIn } = use(AuthContext);
@@ -44,70 +48,96 @@ const SignIn = () => {
 
   return (
     <>
-      <div className="flex justify-center items-center min-h-screen">
-        <div
-          className="card bg-base-100 md:w-3/9 w-3/4 shrink-0 shadow-2xl"
-          style={{ boxShadow: "0 0 50px rgba(0, 119, 182, .6)" }}
-        >
-          <p className="font-semibold text-2xl  md:text-3xl text-center py-8">
-            <span className="text-primary">SignIn</span> to your account
-          </p>
-          <form onSubmit={handleSignIn} className="card-body">
-            <fieldset className="fieldset">
-              <label className="label text-accent">Email</label>
-              <input
-                name="email"
-                type="email"
-                className="input focus:outline-none w-full focus:border-primary md:mb-2"
-                placeholder="Email"
-                required
-              />
-              <label className="label text-accent">Password</label>
-              <input
-                name="password"
-                type="password"
-                className="input focus:outline-none w-full focus:border-primary mb-1 md:mb-2"
-                placeholder="Password"
-                required
-              />
-              <div>
-                <p className="link link-hover">Forgot password?</p>
-              </div>
-              <button className="relative py-2 overflow-hidden font-medium text-primary  border border-primary rounded-l-full rounded-r-full shadow-inner group md:text-lg cursor-pointer">
-                <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-secondary group-hover:w-full ease"></span>
-                <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-secondary group-hover:w-full ease"></span>
-                <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300  bg-secondary group-hover:h-full ease"></span>
-                <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 bg-secondary  group-hover:h-full ease"></span>
-                <span className="absolute inset-0 w-full h-full duration-300 bg-primary opacity-0 group-hover:opacity-100"></span>
-                <span className="relative transition-colors duration-300  group-hover:text-white ease">
-                  SignIn
-                </span>
-              </button>
-              <fieldset className="fieldset border-primary rounded-box border p-4">
-                <legend className="fieldset-legend text-sm text-accent">
-                  Other SignIn options
-                </legend>
-                <p
-                  onClick={handleGoogleSignIn}
-                  className="btn btn-primary btn-outline w-full"
+      <motion.div
+        initial={{ opacity: 0, scale: 0.95 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className="flex justify-center items-center min-h-screen">
+          <div
+            className="card bg-base-100 md:w-3/9 w-5/6 shrink-0 shadow-2xl p-2"
+            style={{ boxShadow: "0 0 50px rgba(0, 119, 182, .6)" }}
+          >
+            <div className="border border-secondary rounded-md">
+              {" "}
+              <div className="flex flex-col gap-2 md:pt-8 pt-4 items-center justify-center ">
+                <GradientText
+                  colors={[
+                    "#40ffaa",
+                    "#4079ff",
+                    "#40ffaa",
+                    "#4079ff",
+                    "#40ffaa",
+                    "#0077B6",
+                  ]}
+                  animationSpeed={5}
+                  showBorder={false}
+                  className="custom-class md:text-4xl text-2xl"
                 >
-                  <FcGoogle size={24} /> SignIn with Google
-                </p>
-                <p className="btn btn-outline btn-primary w-full">
-                  <FaGithub size={24} className="text-black" /> SignIn with
-                  Github
-                </p>
-              </fieldset>
-              <p className="font-semibold text-center pt-4">
-                Don't have an account?<span> </span>
-                <Link className="text-primary" to={"/signUp"}>
-                  SignUp
-                </Link>
-              </p>
-            </fieldset>
-          </form>
+                  SignIn
+                </GradientText>
+                <div className="w-12 md:w-16 ">
+                  <Lottie animationData={signInLottie} loop></Lottie>
+                </div>
+              </div>
+              <form onSubmit={handleSignIn} className="card-body pt-2 md:pt-5">
+                <fieldset className="fieldset">
+                  <label className="label text-accent">Email</label>
+                  <input
+                    name="email"
+                    type="email"
+                    className="input focus:outline-none w-full focus:border-primary md:mb-2"
+                    placeholder="Email"
+                    required
+                  />
+                  <label className="label text-accent">Password</label>
+                  <input
+                    name="password"
+                    type="password"
+                    className="input focus:outline-none w-full focus:border-primary mb-1 md:mb-2"
+                    placeholder="Password"
+                    required
+                  />
+                  <div>
+                    <p className="link link-hover">Forgot password?</p>
+                  </div>
+                  <button className="relative py-2 overflow-hidden font-medium text-primary  border border-primary rounded-l-full rounded-r-full shadow-inner group md:text-lg cursor-pointer">
+                    <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-secondary group-hover:w-full ease"></span>
+                    <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-secondary group-hover:w-full ease"></span>
+                    <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300  bg-secondary group-hover:h-full ease"></span>
+                    <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 bg-secondary  group-hover:h-full ease"></span>
+                    <span className="absolute inset-0 w-full h-full duration-300 bg-primary opacity-0 group-hover:opacity-100"></span>
+                    <span className="relative transition-colors duration-300  group-hover:text-white ease">
+                      SignIn
+                    </span>
+                  </button>
+                  <fieldset className="fieldset border-primary rounded-box border p-4">
+                    <legend className="fieldset-legend text-sm text-accent">
+                      Other SignIn options
+                    </legend>
+                    <p
+                      onClick={handleGoogleSignIn}
+                      className="btn btn-primary btn-outline w-full"
+                    >
+                      <FcGoogle size={24} /> SignIn with Google
+                    </p>
+                    <p className="btn btn-outline btn-primary w-full">
+                      <FaGithub size={24} className="text-black" /> SignIn with
+                      Github
+                    </p>
+                  </fieldset>
+                  <p className="font-semibold text-center pt-4">
+                    Don't have an account?<span> </span>
+                    <Link className="text-primary" to={"/signUp"}>
+                      SignUp
+                    </Link>
+                  </p>
+                </fieldset>
+              </form>
+            </div>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -3,6 +3,10 @@ import { Link, useLocation, useNavigate } from "react-router";
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-toastify";
 import { AuthContext } from "../../providers/AuthContext";
+import { motion } from "framer-motion";
+import signUpLottie from "../../assets/lottie/signupLottie.json";
+import GradientText from "../../animations/GradientText/GradientText";
+import Lottie from "lottie-react";
 
 const SignUp = () => {
   const location = useLocation();
@@ -106,80 +110,110 @@ const SignUp = () => {
       });
   };
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div
-        className="card bg-base-100 md:w-3/9 w-3/4 shrink-0 shadow-2xl"
-        style={{ boxShadow: "0 0 50px rgba(0, 119, 182, .6)" }}
-      >
-        <p className="font-semibold text-2xl  md:text-3xl text-center py-8">
-          <span className="text-primary">SignUp</span> your account
-        </p>
-        <form onSubmit={handleSignUp} className="card-body">
-          <fieldset className="fieldset">
-            <label className="label text-accent">Name</label>
-            <input
-              name="name"
-              type="text"
-              className="input focus:outline-none w-full focus:border-primary md:mb-2"
-              placeholder="Name"
-              required
-            />
-            <label className="label text-accent">Email</label>
-            <input
-              name="email"
-              type="email"
-              className="input focus:outline-none w-full focus:border-primary md:mb-2"
-              placeholder="Email"
-              required
-            />
-            <label className="label text-accent">Photo URL</label>
-            <input
-              name="photo"
-              type="text"
-              className="input focus:outline-none w-full focus:border-primary md:mb-2"
-              placeholder="Photo URL"
-              required
-            />
-            <label className="label text-accent">Password</label>
-            <input
-              name="password"
-              type="password"
-              className="input focus:outline-none w-full focus:border-primary mb-1 md:mb-2"
-              placeholder="Password"
-              required
-            />
-            {<p className="text-red-600 font-medium">{errorMessage}</p>}
-            <button className="relative py-2 overflow-hidden font-medium text-primary  border border-primary rounded-l-full rounded-r-full shadow-inner group md:text-lg cursor-pointer">
-              <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-secondary group-hover:w-full ease"></span>
-              <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-300 border-b-2 border-secondary group-hover:w-full ease"></span>
-              <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300  bg-secondary group-hover:h-full ease"></span>
-              <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 bg-secondary  group-hover:h-full ease"></span>
-              <span className="absolute inset-0 w-full h-full duration-300  bg-primary opacity-0 group-hover:opacity-100"></span>
-              <span className="relative transition-colors duration-300  group-hover:text-white ease">
-                SignUp
-              </span>
-            </button>
-            <fieldset className="fieldset border-primary rounded-box border p-4">
-              <legend className="fieldset-legend text-sm text-accent">
-                Other SignIn options
-              </legend>
-              <p
-                onClick={handleGoogleSignIn2}
-                className="btn btn-primary btn-outline w-full"
-              >
-                <FcGoogle size={24} /> SignIn with Google
-              </p>
-            </fieldset>
-            <p className="font-semibold text-center pt-4">
-              Already have an account?<span> </span>
-              <Link className="text-primary" to={"/signIn"}>
-                SignIn
-              </Link>
-            </p>
-          </fieldset>
-        </form>
+    <motion.div
+      initial={{ opacity: 0, scale: 0.95 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.3 }}
+      className=""
+    >
+      <div className="border md:py-10">
+        {" "}
+        <div className="flex justify-center items-center min-h-screen">
+          <div
+            className="card bg-base-100 md:w-3/9 w-5/6 shrink-0 shadow-2xl p-2"
+            style={{ boxShadow: "0 0 50px rgba(0, 119, 182, .6)" }}
+          >
+            <div className="border border-secondary rounded-md">
+              {" "}
+              <div className="flex flex-col gap-2 md:pt-8 pt-4 items-center justify-center">
+                <GradientText
+                  colors={[
+                    "#40ffaa",
+                    "#4079ff",
+                    "#40ffaa",
+                    "#4079ff",
+                    "#40ffaa",
+                    "#0077B6",
+                  ]}
+                  animationSpeed={5}
+                  showBorder={false}
+                  className="custom-class md:text-4xl text-2xl"
+                >
+                  SignIn
+                </GradientText>
+                <div className="w-12 md:w-16 ">
+                  <Lottie animationData={signUpLottie} loop></Lottie>
+                </div>
+              </div>
+              <form onSubmit={handleSignUp} className="card-body">
+                <fieldset className="fieldset">
+                  <label className="label text-accent">Name</label>
+                  <input
+                    name="name"
+                    type="text"
+                    className="input focus:outline-none w-full focus:border-primary md:mb-2"
+                    placeholder="Name"
+                    required
+                  />
+                  <label className="label text-accent">Email</label>
+                  <input
+                    name="email"
+                    type="email"
+                    className="input focus:outline-none w-full focus:border-primary md:mb-2"
+                    placeholder="Email"
+                    required
+                  />
+                  <label className="label text-accent">Photo URL</label>
+                  <input
+                    name="photo"
+                    type="text"
+                    className="input focus:outline-none w-full focus:border-primary md:mb-2"
+                    placeholder="Photo URL"
+                    required
+                  />
+                  <label className="label text-accent">Password</label>
+                  <input
+                    name="password"
+                    type="password"
+                    className="input focus:outline-none w-full focus:border-primary mb-1 md:mb-2"
+                    placeholder="Password"
+                    required
+                  />
+                  {<p className="text-red-600 font-medium">{errorMessage}</p>}
+                  <button className="relative py-2 overflow-hidden font-medium text-primary  border border-primary rounded-l-full rounded-r-full shadow-inner group md:text-lg cursor-pointer">
+                    <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-secondary group-hover:w-full ease"></span>
+                    <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-300 border-b-2 border-secondary group-hover:w-full ease"></span>
+                    <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300  bg-secondary group-hover:h-full ease"></span>
+                    <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 bg-secondary  group-hover:h-full ease"></span>
+                    <span className="absolute inset-0 w-full h-full duration-300  bg-primary opacity-0 group-hover:opacity-100"></span>
+                    <span className="relative transition-colors duration-300  group-hover:text-white ease">
+                      SignUp
+                    </span>
+                  </button>
+                  <fieldset className="fieldset border-primary rounded-box border p-4">
+                    <legend className="fieldset-legend text-sm text-accent">
+                      Other SignIn options
+                    </legend>
+                    <p
+                      onClick={handleGoogleSignIn2}
+                      className="btn btn-primary btn-outline w-full"
+                    >
+                      <FcGoogle size={24} /> SignIn with Google
+                    </p>
+                  </fieldset>
+                  <p className="font-semibold text-center pt-4">
+                    Already have an account?<span> </span>
+                    <Link className="text-primary" to={"/signIn"}>
+                      SignIn
+                    </Link>
+                  </p>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
